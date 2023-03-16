9a3d1da0c93a7b7b589fc0be63e2d35671e346a1
Quang Nguyen Cong <quang.nguyencong>
2023-03-16 10:59:29 +0700

    Add Unit Test for Customer

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -2,21 +2,76 @@
 
 namespace Tests\Feature;
 
+use App\Models\SchedulePayment;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CustomerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic feature test example.
+     * Customer can not create loan if not login.
      *
      * @return void
      */
-    public function test_example()
+    public function test_that_customer_can_not_access_creat_loan_if_not_login()
     {
-        $response = $this->get('/');
+        $response =  $this->withHeaders(['Accept' => 'application/json'])->post('/api/customer/createLoan', ['term' => 3, 'amount' => 10000]);
 
-        $response->assertStatus(200);
+        $response->assertStatus(401);
+        $response->assertExactJson(['message' => 'Unauthenticated.']);
+    }
+
+    /**
+     * Customer can create loan with correct token.
+     *
+     * @return void
+     */
+    public function test_that_customer_can_creat_loan_with_corect_token()
+    {
+        $this->seed();
+        $token = $this->getCustomerToken();
+
+
+        $response =  $this->createLoan($token);
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('message', 'Create Loan Successfully')
+            ->assertJsonPath('loan.id', 1)
+            ->assertJsonPath('loan.status', 'PENDING');
+
+        $schedulePayments = SchedulePayment::where('loan_id', 1)->get();
+        foreach ($schedulePayments as $schedulePayment) {
+            $this->assertEquals('PENDING', $schedulePayment->status);
+        }
+    }
+
+    public function test_that_customer_can_view_loan_belongs_to_him()
+    {
+        $this->seed();
+        $token = $this->getCustomerToken();
+
+        $response = $this->createLoan($token);
+        $loan = json_decode($response->getContent(), true);
+        $loanId = $loan['loan']['id'];
+
+        $viewLoanResponse = $this->withHeaders(['Accept' => 'application/json', 'Bearer Token' => $token])
+            ->get('api/customer/loan/'.$loanId);
+
+        $viewLoanResponse->assertStatus(200);
+    }
+
+    private function getCustomerToken()
+    {
+        $response = $this->post('/api/auth/login', ['email' => 'customer@aspire.com', 'password' => 'customer@#$%']);
+        $response = json_decode($response->getContent(), true);
+        return $response['token'];
+    }
+
+    private function createLoan($token)
+    {
+        return $this->withHeaders(['Accept' => 'application/json', 'Bearer Token' => $token])
+            ->post('/api/customer/createLoan', ['term' => 3, 'amount' => 10000]);
     }
 }
--- tests/Unit/LoanServiceTest.php
+++ tests/Unit/LoanServiceTest.php
@@ -2,17 +2,42 @@
 
 namespace Tests\Unit;
 
+use App\Services\LoanService;
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 
 class LoanServiceTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test schedule repayment.
      *
      * @return void
      */
-    public function test_example()
+    public function test_schedule_repayment_can_generate_correctly()
     {
-        $this->assertTrue(true);
+        $loanService = new LoanService();
+        $loanDate = new Carbon('2022-02-07');
+        $term = 3;
+        $amount = 10000;
+
+        $scheduleRepayments = $loanService->scheduleRepayment($loanDate, $term, $amount);
+        $expectedScheduleRepayments = [
+            [
+                'due_date' => '2022-02-14',
+                'amount_format' => '3.333,33',
+                'amount' => 3333.33
+            ],
+            [
+                'due_date' => '2022-02-21',
+                'amount_format' => '3.333,33',
+                'amount' => 3333.33
+            ],
+            [
+                'due_date' => '2022-02-28',
+                'amount_format' => '3.333,34',
+                'amount' => 3333.34
+            ]
+        ];
+        $this->assertEquals(json_encode($scheduleRepayments), json_encode($expectedScheduleRepayments));
     }
 }
